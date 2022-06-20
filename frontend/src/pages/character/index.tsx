57df62887344e6b53c413e6530762477d1fc49d2
Lance Wells <lance.a.wells@gmail.com>
2022-06-19 18:39:24 -0700

    Updated tiltfile watchers.

---
+++ frontend/src/pages/character/index.tsx
@@ -0,0 +1,10 @@
+export type CharacterViewProps = {
+}
+
+export default function CharacterView(props: CharacterViewProps) {
+  return (
+    <div>
+      <h1>This is a placeholder page.</h1>
+    </div>
+  )
+}

d4c0396f86938dd87c2ad2a2e4ceabb1d724abbd
Lance Wells <lance.a.wells@gmail.com>
2022-06-26 12:22:07 -0700

    Updated migration for charimagedb.

--- backend/characterimage/config/base/migrations/001_create_base.up.sql
+++ backend/characterimage/config/base/migrations/001_create_base.up.sql
@@ -11,6 +11,13 @@ CREATE TYPE static_part_type AS ENUM (
   'HAIR'
 );
 
+CREATE TYPE prop_type AS ENUM (
+  'WEAPON_MELEE_1H',
+  'WEAPON_MELEE_2H',
+  'WEAPON_RANGE_1H',
+  'WEAPON_RANGE_2H'
+);
+
 CREATE TABLE color_string (
   id SERIAL PRIMARY KEY,
   hexstring varchar(9)
@@ -21,48 +28,84 @@ CREATE TABLE body_type (
   display_name varchar NOT NULL
 );
 
-CREATE TABLE body_type_pixel (
-  part_type dynamic_part_type,
-  x smallint,
-  y smallint,
-  color_string_id int REFERENCES color_string,
-  body_type_id int REFERENCES body_type(id),
-  PRIMARY KEY(color_string_id, body_type_id),
-  UNIQUE(x, y, body_type_id)
-);
-
 CREATE TABLE dynamic_part (
   id SERIAL PRIMARY KEY,
+  body_type_id int REFERENCES body_type(id),
   display_name varchar NOT NULL,
-  part_type dynamic_part_type,
-  body_type_id int REFERENCES body_type(id)
+  part_type dynamic_part_type
 );
 
 CREATE TABLE dynamic_part_pixel (
-  x smallint,
-  y smallint,
   color_string_id int REFERENCES color_string,
   dynamic_part_id int REFERENCES dynamic_part(id),
+  x smallint,
+  y smallint,
   UNIQUE(x, y, dynamic_part_id),
   PRIMARY KEY(color_string_id, dynamic_part_id)
 );
 
 CREATE TABLE static_part (
   id SERIAL PRIMARY KEY,
+  body_type_id int REFERENCES body_type(id),
   display_name varchar NOT NULL,
-  part_type static_part_type,
-  image_bytes bytea,
-  x smallint,
-  y smallint,
-  body_type_id int REFERENCES body_type(id)
+  part_type static_part_type
 );
 
-CREATE TABLE dynamic_part_colormap_pixel (
-  part_type dynamic_part_type,
+CREATE TABLE static_part_image (
+  static_part_id int REFERENCES static_part(id) PRIMARY KEY,
   x smallint,
   y smallint,
+  image_bytes bytea
+);
+
+CREATE TABLE prop (
+  id SERIAL PRIMARY KEY,
+  display_name varchar NOT NULL,
+  part_type prop_type
+);
+
+CREATE TABLE prop_image (
+  prop_id int REFERENCES prop(id) PRIMARY KEY,
+  x smallint,
+  y smallint,
+  image_bytes bytea
+);
+
+CREATE TABLE animation (
+  id SERIAL PRIMARY KEY,
+  body_type_id int REFERENCES body_type(id),
+  display_name varchar NOT NULL,
+  part_type prop_type[]
+);
+
+CREATE TABLE animation_frame (
+  id SERIAL PRIMARY KEY,
+  animation_id int REFERENCES animation(id),
+  frame_index int NOT NULL,
+  UNIQUE(animation_id, frame_index)
+);
+
+CREATE TABLE animation_frame_pixel (
   color_string_id int REFERENCES color_string,
-  dynamic_part_id int REFERENCES dynamic_part(id),
-  PRIMARY KEY(color_string_id, dynamic_part_id),
-  UNIQUE(x, y, dynamic_part_id)
+  animation_frame_id int REFERENCES animation_frame(id),
+  x smallint,
+  y smallint,
+  UNIQUE(x, y, animation_frame_id),
+  PRIMARY KEY(color_string_id, animation_frame_id)
+);
+
+CREATE TABLE animation_frame_static_position (
+  animation_frame_id int REFERENCES animation_frame(id),
+  part_type static_part_type,
+  x smallint,
+  y smallint,
+  PRIMARY KEY(animation_frame_id, part_type)
+);
+
+CREATE TABLE animation_frame_prop_position (
+  animation_frame_id int REFERENCES animation_frame(id),
+  part_type prop_type,
+  x smallint,
+  y smallint,
+  PRIMARY KEY(animation_frame_id, part_type)
 );
